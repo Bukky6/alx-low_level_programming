29ab892d465eb467f7597e7397bba9c6794aacca
Bukky6 <the>
2023-02-17 04:55:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - A program that prints without function puts and printf
+ * main - A program that prints without function puts and printf"
  *
  * Return: 1 if success
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
+}
